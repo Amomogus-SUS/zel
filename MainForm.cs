91fb454d2709ea17fe08c2ce4cc489a8efb4bc40
Niten't <156431941+Nitent@users.noreply.github.com>
2024-09-03 12:17:17 +0300

    Initial commit

---
+++ MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using zel.DBContext;
+
+namespace zel
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        Model1 model = new Model1();
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = model.Users.ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddUsers form = new AddUsers();
+            form.ShowDialog();
+            dataGridView1.DataSource = model.Users.ToList();
+        }
+    }
+}
